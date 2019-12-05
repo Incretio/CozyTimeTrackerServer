e7560cb627b7d1f3708f70f569822e105583ba30
oleg rodin <oleg.rodin@gridnine.com>
2019-12-05 07:30:02 +0300

    add destroy method in ContextListener

--- src/main/java/com/incretio/cozy_time_tracker_server/config/SpringApplicationContextListener.java
+++ src/main/java/com/incretio/cozy_time_tracker_server/config/SpringApplicationContextListener.java
@@ -1,6 +1,5 @@
 package com.incretio.cozy_time_tracker_server.config;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.servlet.ServletContextEvent;
@@ -8,10 +7,17 @@ import javax.servlet.ServletContextListener;
 
 public class SpringApplicationContextListener implements ServletContextListener {
 
+    private ClassPathXmlApplicationContext applicationContext;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        applicationContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
         sce.getServletContext().setAttribute("applicationContext", applicationContext);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        applicationContext.close();
     }
 
 }
